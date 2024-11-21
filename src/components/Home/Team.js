9f550a709448609955ab21b174bc6c0c12a35419
yazin123 <mohammedyazin4@gmail.com>
2024-11-21 10:06:25 +0530

    team changes

--- src/components/Home/Team.js
+++ src/components/Home/Team.js
@@ -13,15 +13,19 @@ const TeamBoard = () => {
   ];
 
   const boardMembers = [
-    { name: 'Dr. Thomas George K', role: 'Director at Lead College', image: '/members/Image.jpg' },
     { name: 'Mr. Renjith Raj', role: 'Founder - Paddle Consultants', image: '/members/renjith.jpg' },
-    { name: 'Mr. Mathew Joseph', role: 'Founder - FreshToHome', image: '/members/Image.jpg' },
-    { name: 'Mr. Jobin S Kottaram', role: 'Founder - Absolute IAS Academy', image: '/members/Image.jpg' },
-    { name: 'Mr. Prasobh P', role: 'CEO Hemito Digital', image: '/members/Image.jpg' },
     { name: 'Mr. Subilal K', role: 'Automation Coach', image: '/members/subilal.jpg' },
-    { name: 'Mr. Suresh Kumar K', role: 'Founder - Skybertech', image: '/members/Image.jpg' },
-    { name: 'Mr. Madhu Bhaskaran', role: 'Director Samagra', image: '/members/Image.jpg' },
-    { name: 'Mr. Shihabudheen', role: 'AI Consultant', image: '/members/Image.jpg' },
+    { name: 'Dr. Ramesh K V', role: 'Public speaking coach', image: '/members/rameshs.jpg' },
+    { name: 'Mr. Sunil Mathai', role: 'Founder - TradeTalks', image: '/members/akhil.jpg' },
+    { name: 'Dr. Arun Oommen', role: 'Senior Consultant Neurosurgeon - VPS Lakeshore Hospital', image: '/members/arun.jpg' },
+    { name: 'Mr. Shihabudheen P K', role: 'CEO & Co-Founder,Hash Future School', image: '/members/shihab.jpg' },
+    { name: 'Mr. Prasobh P', role: 'CEO Hemito Digital', image: '/members/Image.jpg' },
+    { name: 'Mr. Mujeeb Rahaman', role: 'Influencer ,Teacher My vlog', image: '/members/mujeeb.jpg' },
+    { name: 'Dr. Thomas George K', role: 'Director - LEAD College of Management', image: '/members/thomas.jpg' },
+    { name: 'Mr. Rashid K A', role: 'Director - Ajmi Foods', image: '/members/rashid.jpg' },
+    { name: 'Mr. Prabeesh', role: 'COO Padle Group', image: '/members/prabeesh.jpg' },
+    { name: 'Mr. Denny Tomy', role: 'Managing Partner - Loanitol', image: '/members/denny.jpg' },
+
   ];
 
   return (
@@ -35,7 +39,7 @@ const TeamBoard = () => {
           <div key={index} className=" backdrop-blur-sm rounded-lg p-6 md:flex  md:flex-row gap-6 items-center" data-aos="fade-in">
             <div className='flex justify-center'>
               <div className="w-48 h-48 md:w-56 md:h-56 flex justify-center">
-                <Image src={executive.image} className="w-full h-full bg-gray-300 rounded-lg object-cover" width={400} height={400} />
+                <Image src={executive.image} className="w-full h-full bg-gray-300 rounded-lg object-cover " width={400} height={400} />
               </div>
             </div>
             <div className="flex-1 text-center md:text-left">
@@ -51,11 +55,11 @@ const TeamBoard = () => {
       {/* Board of Advisors Section */}
       <section>
         <h2 className="text-3xl md:text-4xl text-white mb-8 font-light" data-aos="fade-in">Board of Advisors</h2>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2">
           {boardMembers.map((member, index) => (
             <div key={index} className=" backdrop-blur-sm rounded-lg p-4 flex flex-col items-center transition-transform hover:scale-105" data-aos="fade-in">
-              <div className="w-32 h-32 md:w-40 md:h-40 mb-4">
-                <Image className="w-full h-full bg-gray-300 rounded-lg" src={member.image} width={300} height={300} />
+              <div className="w-32 h-32  md:h-52 md:w-52 mb-4">
+                <Image className="w-full h-full bg-gray-300 rounded-lg object-cover" src={member.image} width={300} height={300} />
               </div>
               <h3 className="text-lg text-white font-semibold text-center">{member.name}</h3>
               <p className="text-purple-200 text-sm text-center mt-1">{member.role}</p>
